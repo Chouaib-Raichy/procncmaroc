03d57067f40eb3629837bcea74fed7a30c874cd3
Chouaib Raichy <chouaib@procncmaroc.com>
2026-06-23 16:09:34 +0100

    Async CSS loading (eliminates render-blocking)
    - Vite plugin converts <link rel=stylesheet> to preload+onload pattern
    
    - CSS downloads immediately but doesn't block FCP/LCP
    
    - Saves ~300ms on slow connections

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'async-css',
+      transformIndexHtml(html) {
+        return html.replace(
+          /<link rel="stylesheet" crossorigin href="([^"]+\.css)">/,
+          `<link rel="preload" as="style" href="$1" onload="this.onload=null;this.rel='stylesheet'">\n    <noscript><link rel="stylesheet" crossorigin href="$1"></noscript>`
+        );
+      },
+    },
+  ],
   appType: 'spa',
   server: { port: 3000 },
   build: {
